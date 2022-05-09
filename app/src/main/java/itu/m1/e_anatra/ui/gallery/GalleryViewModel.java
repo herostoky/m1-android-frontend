d40d4c0798af12a823700c2c63ffc60a7d452e74
Toky <Toky.SOLOFONIRINA@pulse.mg>
2022-05-09 14:29:30 +0300

    Init android project

---
+++ app/src/main/java/itu/m1/e_anatra/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package itu.m1.e_anatra.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
